f90a48b6f2f79c6271f1f9a9ee582691d6c27df1
Bismark Ankamah <Bismarka2009@hotmail.com>
2023-12-14 11:57:45 -0500

    Testing

---
+++ ecr.tf
@@ -0,0 +1,14 @@
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "ecr_test"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  tags = {
+    Name  = "app-repository"
+    Group = "test"
+  }
+}
