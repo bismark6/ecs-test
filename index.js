a4df0225efd600947e0868d8d9f10b9862aa8a36
Bismark Ankamah <Bismarka2009@hotmail.com>
2023-12-15 13:31:03 -0500

    Test 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res){
-    res.send("Hello World Test Test");
+    res.send("Hello World");
 });
 
-app.listen(80);
+app.listen(8080);
